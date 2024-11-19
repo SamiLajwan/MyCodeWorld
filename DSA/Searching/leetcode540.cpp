7b2a5d9bc8be23982d355385e0164f57f25b2e91
Abdul Sami Lajwan <sami.lajwan3121@gmail.com>
2024-11-20 02:43:58 +0530

    Leetcode 540

---
+++ DSA/Searching/leetcode540.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& arr) {
+        int s = 0;
+        int n = arr.size();
+        int e = n - 1;
+        int mid;
+
+        while (s < e) {
+            mid = s + (e - s) / 2;
+
+            // Check if mid is part of a pair
+            if (mid % 2 == 0) {
+                if (arr[mid] == arr[mid + 1]) {
+                    // Move to the right part
+                    s = mid + 2;
+                } else {
+                    // Move to the left part
+                    e = mid;
+                }
+            } else {
+                if (arr[mid] == arr[mid - 1]) {
+                    // Move to the right part
+                    s = mid + 1;
+                } else {
+                    // Move to the left part
+                    e = mid - 1;
+                }
+            }
+        }
+
+        // When s == e, we've found the single non-duplicate element
+        return arr[s];
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> arr = { 2, 2, 3, 3, 4, 4, 8};
+
+    int ans = sol.singleNonDuplicate(arr);
+
+    cout << "Single Non-Duplicate Element: " << ans << endl;
+
+    return 0;
+}
