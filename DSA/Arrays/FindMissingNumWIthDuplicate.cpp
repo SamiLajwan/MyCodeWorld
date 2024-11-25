9592b0810c5b8c3817d929cf1cf2e1c86c12b405
Abdul Sami Lajwan <sami.lajwan3121@gmail.com>
2024-11-25 23:33:42 +0530

    Access Code.

---
+++ DSA/Arrays/FindMissingNumWIthDuplicate.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cmath> // For abs function
+using namespace std;
+
+void findMissing(int *a, int n){
+    for(int i = 0; i < n; i++){
+        int index = abs(a[i]);
+        if(index - 1 < n && a[index - 1] > 0){ // Ensure index is within bounds
+            a[index - 1] *= -1;               // Mark as visited
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        if(a[i] > 0){
+            cout << i + 1 << " ";             // Print missing numbers
+        }
+    }
+}
+
+int main(){
+    int n;
+    int a[] = {1, 2, 3, 3, 3};  
+    n = sizeof(a) / sizeof(int);
+
+    findMissing(a, n);
+    return 0;
+}
